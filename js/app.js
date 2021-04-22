e1a329b044c62fa1d8d2062fd11133003e3ad9f1
Emmanuel Cuevas <emmanuelcuevasm1@gmail.com>
2021-04-22 16:54:21 -0400

    BE login added

--- js/app.js
+++ js/app.js
@@ -9,3 +9,54 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+
+
+
+function Login(){
+
+  var email= document.getElementById("email").value;
+  var password= document.getElementById("pass").value;
+
+  firebase.auth().signInWithEmailAndPassword(email,password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    window.location.href = 'Index.html';
+    console.log("logged");
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("error:"  + errorMessage);
+    console.log("error al loggear");
+  });
+  
+
+  
+}
+
+
+function SignUp(){
+
+  var email= document.getElementById("newEmail").value;
+  var password= document.getElementById("password").value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+
+    window.alert("error:" + errorMessage);
+  });
+
+
+  
+}
