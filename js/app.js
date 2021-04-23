7905e860cee88ff92407a519f048dc3c44f0ec18
Emmanuel Cuevas <emmanuelcuevasm1@gmail.com>
2021-04-23 17:44:47 -0400

    login fixed

--- js/app.js
+++ js/app.js
@@ -1,62 +1,66 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
-
 sign_up_btn.addEventListener("click", () => {
+  
   container.classList.add("sign-up-mode");
-});
 
+  
+
+
+  });
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
-});
+  });
 
 
 
 
 function Login(){
-
-  var email= document.getElementById("email").value;
-  var password= document.getElementById("pass").value;
-
+    
+    var email= document.getElementById("email").value;
+    var password= document.getElementById("pass").value;
+  
   firebase.auth().signInWithEmailAndPassword(email,password)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    window.location.href = 'Index.html';
-    console.log("logged");
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    window.alert("error:"  + errorMessage);
-    console.log("error al loggear");
-  });
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      window.location.href = 'Index.html';
+      console.log("logged");
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error:"  + errorMessage);
+      console.log("error al loggear");
+    });
+    
   
-
+    
+  }
   
-}
-
-
-function SignUp(){
-
-  var email= document.getElementById("newEmail").value;
-  var password= document.getElementById("password").value;
-
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ..
-
-    window.alert("error:" + errorMessage);
-  });
-
-
   
-}
+  function SignUp(){
+  
+    var email= document.getElementById("newEmail").value;
+    var password= document.getElementById("password").value;
+  
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      window.alert("Bienvenido" + user.email);
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+  
+      window.alert("Error:" + errorMessage);
+    });
+  
+  
+  }
+  
